05e66bdc3319ad6fd8968295b159254651b25cd5
Matthew Stratford <matthew@mstratford.net>
2017-04-20 02:39:00 +0100

    Prevent stream from being delayed from paused state.

--- assets/player.js
+++ assets/player.js
@@ -3,6 +3,9 @@
 
 var music = document.getElementById("music"); // id for audio element
 
+var sourceElement = document.querySelector("source");
+var originalSourceUrl = sourceElement.getAttribute("src");
+
 $(document).ready(function() {
   // variable to store HTML5 audio element
   play();
@@ -90,12 +93,21 @@ function play() {
   // start music
   volumeUpdate();
   if (music.paused) {
+    if (!sourceElement.getAttribute("src")) {
+      sourceElement.setAttribute("src", originalSourceUrl);
+      music.load(); // This restarts the stream download
+    }
     music.play();
     // remove play, add pause
     pButton.className = "";
     pButton.className = "col-2 fa fa-pause";
   } else { // pause music
+    sourceElement.setAttribute("src", ""); //prevent's being delayed on play again.
     music.pause();
+    // settimeout, otherwise pause event is not raised normally
+    setTimeout(function () {
+      music.load(); // This stops the stream from downloading
+    });
     // remove pause, add play
     pButton.className = "";
     pButton.className = "col-2 fa fa-play";
